cb8bafef65e1cf90170efd012dee2768ddfbc2c9
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2026-02-14 12:17:21 +0100

    build(server): add postgres dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,12 +20,16 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation(libs.springdoc.openapi)
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(libs.springdoc.openapi)
     implementation(libs.jwt)
+    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
