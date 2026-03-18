aeb6c50bba7d1511cbbba35d8f57ca7ca17a91fa
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2026-03-18 19:02:25 +0100

    test(server): added test for creation user endpoint

--- server/src/test/kotlin/org/example/server/adapters/controllers/LoginControllerTest.kt
+++ server/src/test/kotlin/org/example/server/adapters/controllers/LoginControllerTest.kt
@@ -2,19 +2,23 @@ package org.example.server.adapters.controllers
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.kotest.core.spec.style.FunSpec
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.example.server.application.ports.CreateUserOutputBoundary
 import org.example.server.application.ports.LoginOutputBoundary
 import org.example.server.application.ports.UserInputBoundary
 import org.example.server.application.ports.models.login.LoginRequestModel
 import org.example.server.application.ports.models.login.LoginResponseModel
+import org.example.server.application.ports.models.user.UserResponseModel
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import java.time.LocalDateTime
 
 @WebMvcTest(LoginController::class)
 class LoginControllerTest :
@@ -24,6 +28,36 @@ class LoginControllerTest :
         val objectMapper = ObjectMapper()
         val mockMvc: MockMvc = MockMvcBuilders.standaloneSetup(controller).build()
 
+        test("create user success return response dto") {
+            val responseModel =
+                UserResponseModel(username = "Alice", token = "token123", time = LocalDateTime.now().toString())
+            every { userInputBoundary.createUser(any(), any()) } answers {
+                val presenter = secondArg<CreateUserOutputBoundary>()
+                presenter.presentSuccess(responseModel)
+            }
+
+            val request = mapOf("username" to "Alice", "password" to "pass")
+            val json = objectMapper.writeValueAsString(request)
+
+            mockMvc
+                .perform(
+                    MockMvcRequestBuilders
+                        .post("/api/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json),
+                ).andExpect(MockMvcResultMatchers.status().isCreated)
+                .andExpect { MockMvcResultMatchers.jsonPath("$.token").value("token123") }
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("Alice"))
+
+            verify {
+                userInputBoundary.createUser(
+                    match { it.username == "Alice" && it.password == "pass" },
+                    any(),
+                )
+            }
+            confirmVerified(userInputBoundary)
+        }
+
         test("login success returns response dto") {
             val responseModel = LoginResponseModel(username = "Alice", token = "token123")
             every { userInputBoundary.login(any<LoginRequestModel>(), any()) } answers {
@@ -31,7 +65,7 @@ class LoginControllerTest :
                 presenter.presentSuccess(responseModel)
             }
 
-            val request = mapOf("username" to "user1", "password" to "pass")
+            val request = mapOf("username" to "Alice", "password" to "pass")
             val json = objectMapper.writeValueAsString(request)
 
             mockMvc
@@ -44,7 +78,13 @@ class LoginControllerTest :
                 .andExpect(MockMvcResultMatchers.jsonPath("$.token").value("token123"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("Alice"))
 
-            verify { userInputBoundary.login(any(), any()) }
+            verify {
+                userInputBoundary.login(
+                    match { it.username == "Alice" && it.password == "pass" },
+                    any(),
+                )
+            }
+            confirmVerified(userInputBoundary)
         }
 
         test("login failure returns unauthorized and message") {
@@ -53,7 +93,7 @@ class LoginControllerTest :
                 presenter.presentInvalidCredentials("bad creds")
             }
 
-            val request = mapOf("username" to "user1", "password" to "wrong")
+            val request = mapOf("username" to "Alice", "password" to "wrong")
             val json = objectMapper.writeValueAsString(request)
 
             mockMvc
@@ -65,6 +105,12 @@ class LoginControllerTest :
                 ).andExpect(MockMvcResultMatchers.status().isUnauthorized)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("bad creds"))
 
-            verify { userInputBoundary.login(any(), any()) }
+            verify {
+                userInputBoundary.login(
+                    match { it.username == "Alice" && it.password == "wrong" },
+                    any(),
+                )
+            }
+            confirmVerified(userInputBoundary)
         }
     })
